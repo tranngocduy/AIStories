381267f17d5489390b32a611ccf10b953daf82d2
MDEV <tranngocduy.it@gmail.com>
2025-10-08 15:02:18 +0700

    [App] update UI

--- src/screen/Tab.Dashboard/index.tsx
+++ src/screen/Tab.Dashboard/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
+
+import ScrollRefresh from '@app-component/ScrollRefresh';
 
 import Group from './component/Group';
 
@@ -7,10 +9,20 @@ import styles from './styles';
 
 const Dashboard = () => {
 
+  const _onRefresh = async () => {
+
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.view}>
+        <ScrollView contentContainerStyle={styles.scroll} refreshControl={<ScrollRefresh onRefresh={_onRefresh} />}>
 
+          <View style={styles.separator} />
+
+          <View style={styles.separator} />
+
+        </ScrollView>
       </View>
     </View>
   )
