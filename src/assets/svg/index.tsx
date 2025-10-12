cc63d76eeaef3e9f9a6b1d351d3010e6a2db2176
MDEV <tranngocduy.it@gmail.com>
2025-10-12 21:51:09 +0700

    [App] update UI

---
+++ src/assets/svg/index.tsx
@@ -0,0 +1,7 @@
+import { SvgProps } from 'react-native-svg';
+
+import IDashboard_SVG from './resource/iDashboard.svg';
+export const IDashboardSVG = (props: SvgProps) => <IDashboard_SVG {...props} />;
+
+import IUserProfile_SVG from './resource/iUserProfile.svg';
+export const IUserProfileSVG = (props: SvgProps) => <IUserProfile_SVG {...props} />;
