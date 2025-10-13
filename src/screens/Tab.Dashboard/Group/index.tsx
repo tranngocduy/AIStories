ee4b525af24540750b566ca634b7d0ab352762da
MDEV <tranngocduy.it@gmail.com>
2025-10-13 12:14:08 +0700

    [App] update UI

--- src/screens/Tab.Dashboard/Group/index.tsx
+++ src/screens/Tab.Dashboard/Group/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, FlatList } from 'react-native';
+
+import { TStory } from '@/models/types';
 
 import { TextBase } from '@/components/TextBase';
+import { StoryItem } from '@/components/StoryItem';
 import { TouchableView } from '@/components/TouchableView';
 
 import { styles } from './styles';
@@ -9,9 +12,13 @@ import { TGroupProps } from '../types';
 
 export const Group: React.FC<TGroupProps> = ({ label, data }) => {
 
-  const _onPressSeeAll = () => {
+  const items = data || new Array(10).fill('');
 
-  }
+  const _onPressSeeAll = () => { }
+
+  const _keyExtractor = (item: TStory, index: number) => `${item?.id || index}`;
+
+  const _renderItem = ({ item }: { item: TStory }) => <StoryItem item={item} isHorizon={true} />;
 
   return (
     <View style={styles.container}>
@@ -20,6 +27,14 @@ export const Group: React.FC<TGroupProps> = ({ label, data }) => {
         {!!data?.[0] && <TouchableView hitSlop={12} onPress={_onPressSeeAll}><TextBase style={styles.seeMore}>Xem tất cả</TextBase></TouchableView>}
       </View>
 
+      <FlatList
+        data={items}
+        horizontal={true}
+        renderItem={_renderItem}
+        keyExtractor={_keyExtractor}
+        contentContainerStyle={styles.scroll}
+        showsHorizontalScrollIndicator={false}
+      />
     </View>
   )
 
