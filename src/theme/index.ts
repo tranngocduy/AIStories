48750ada9a48fb1fe284dfc1cd5a2f28c4e47de7
MDEV <tranngocduy.it@gmail.com>
2025-10-13 09:29:42 +0700

    [App] update UI

---
+++ src/theme/index.ts
@@ -0,0 +1,25 @@
+import { Dimensions, StatusBar, TextStyle } from 'react-native';
+import { hasNotch, hasDynamicIsland } from 'react-native-device-info';
+
+import { isIOS } from '@/utils/app';
+
+const { width, height } = Dimensions.get('window');
+const isDynamicIsland = !!isIOS && hasDynamicIsland();
+
+export const wWidth = width;
+export const wHeight = height;
+
+export const isIphoneX = ((!!isIOS && hasNotch()) || !!isDynamicIsland);
+export const statusHeight = !isIOS ? ((StatusBar?.currentHeight || 32) + 8) : !!isDynamicIsland ? 54 : !!isIphoneX ? 48 : 22;
+
+export const headerHeight = 52;
+export const footerHeight = !!isIphoneX ? 24 : 16;
+export const styleProps = { flex: 1, backgroundColor: '#FFFFFF' };
+
+export const fonts = {
+  default: {
+    normal700: { fontWeight: '700' } as TextStyle,
+    normal500: { fontWeight: '500' } as TextStyle,
+    normal400: { fontWeight: '400' } as TextStyle
+  }
+}
