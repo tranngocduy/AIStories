4325b14288695d9753c33673e3c8d3ccc7c28db6
MDEV <tranngocduy.it@gmail.com>
2025-10-20 16:32:28 +0700

    [App] update UI

--- src/instance/components/SearchStories/PageSort/index.tsx
+++ src/instance/components/SearchStories/PageSort/index.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { View, ScrollView } from 'react-native';
 
-import { TPageFilterProps } from '../types';
+import { FILTER_OPTION_SORT } from '@/constants';
+
+import { TextBase } from '@/components/TextBase';
+import { OptionFilter } from '@/components/OptionFilter';
+import { TouchableView } from '@/components/TouchableView';
+
+import { styles } from './styles';
+import { TPageFilterProps, TOptionFilter } from '../types';
 
 export const PageSort: React.FC<TPageFilterProps> = ({ onChangeFilter }) => {
 
-  return (
-    <View>
+  const items = [...FILTER_OPTION_SORT];
 
+  const [selected, setSelected] = useState<TOptionFilter>(items[0]);
+
+  const _onSelect = (value: TOptionFilter) => setSelected(value);
+
+  const _onPressApply = () => onChangeFilter({ type: 'sort', ...selected });
+
+  const _renderItem = (option: TOptionFilter, index: number) => {
+    const isActive = (option.value === selected.value);
+
+    return <OptionFilter option={option} isActive={isActive} onSelect={_onSelect} key={index} />;
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.view}><ScrollView contentContainerStyle={styles.scroll}>{items.map(_renderItem)}</ScrollView></View>
+
+      <View style={styles.bottom}><TouchableView hitSlop={12} onPress={_onPressApply}><TextBase style={styles.bottomText}>Áp dụng điều kiện lọc</TextBase></TouchableView></View>
     </View>
   )
 
