a8fda12c8710488619ea45d216db4afd1cda734b
MDEV <tranngocduy.it@gmail.com>
2025-10-22 13:44:04 +0700

    [App] update UI

---
+++ src/useQuery/useStoryMarked.ts
@@ -0,0 +1,18 @@
+import { ServiceAPI } from '@/apis';
+import { useQuery, QUERY_KEYS } from '@/useQuery/constants';
+
+const _loadData = async () => {
+  const result = await ServiceAPI.getStoryMarked();
+
+  return (result?.data || {});
+}
+
+export const useStoryMarked = ({ enabled = true }: { enabled?: boolean }) => {
+  const query = useQuery({
+    queryKey: [QUERY_KEYS.GET_STORY_MARKED],
+    queryFn: async () => await _loadData(),
+    enabled
+  });
+
+  return query;
+};
