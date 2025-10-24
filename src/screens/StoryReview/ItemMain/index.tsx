c558d2e0a3ced5ef0c8a29059f27a34b8af5233f
MDEV <tranngocduy.it@gmail.com>
2025-10-24 15:40:58 +0700

    [App] update UI

---
+++ src/screens/StoryReview/ItemMain/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { dayjs } from '@/utils/timeTz';
+import { LABEL_REVIEW } from '@/constants';
+import { TStoryRateVotes } from '@/models/types';
+
+import { TextBase } from '@/components/TextBase';
+import { StoryScore } from '@/components/StoryScore';
+
+import { styles } from './styles';
+
+type TItemMainProps = { data: TStoryRateVotes };
+
+export const ItemMain: React.FC<TItemMainProps> = ({ data }) => {
+
+  const score = data?.score;
+
+  const usernameReview = data?.user?.username || '';
+
+  const statusReview = LABEL_REVIEW?.[(score - 1)] || '';
+
+  const content = data?.content?.replace(/<(.|\n)*?>/g, '')?.trim?.();
+
+  const createAt = !!data?.created_at ? dayjs(data?.created_at).format('DD/MM/YYYY') : '';
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.view}>
+        <View style={styles.infoView}>
+          <View style={styles.usernameView}><TextBase style={styles.username} numberOfLines={1}>{usernameReview}</TextBase></View>
+          <StoryScore score={score} isHideScore={true} />
+          <TextBase style={styles.status}>{statusReview}</TextBase>
+        </View>
+        <TextBase style={styles.createAt}>{createAt}</TextBase>
+      </View>
+
+      <View style={styles.separator} />
+
+      <TextBase style={styles.content}>{content}</TextBase>
+    </View>
+  )
+
+}
--- src/screens/StoryReview/index.tsx
+++ src/screens/StoryReview/index.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useMemo } from 'react';
+import { View, FlatList } from 'react-native';
+
+import { useRouteNavigation } from '@/useHooks/useNavigation';
+
+import { HeaderStack } from '@/components/HeaderStack';
+import { ScrollAvoidingView } from '@/components/ScrollAvoidingView';
+
+import { ItemMain } from './ItemMain';
+
+import { styles } from './styles';
 
 export const StoryReview: React.FC<{}> = () => {
 
-  return (
-    <View>
+  const { params } = useRouteNavigation('StoryReview');
 
+  const id = params.review.id;
+
+  const content = params.review.content;
+
+  const _viewsHeader = useMemo(() => <ItemMain data={params.review} />, []);
+
+  return (
+    <View style={styles.container}>
+      <HeaderStack label='Chi tiết bình luận' />
+
+      <ScrollAvoidingView>
+        <FlatList
+          ListHeaderComponent={_viewsHeader}
+          contentContainerStyle={styles.scroll}
+        />
+      </ScrollAvoidingView>
     </View>
   )
 
