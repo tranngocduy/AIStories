9ea6ef571ea5bb2050dc131ec9d4134d91cd3eb3
MDEV <tranngocduy.it@gmail.com>
2026-01-08 22:28:46 +0700

    [App] update UI

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator, NativeStackNavigationOptions } from '@react-navigation/native-stack';
 import { createBottomTabNavigator, BottomTabNavigationOptions, BottomTabBarProps } from '@react-navigation/bottom-tabs';
 
+import Library from '@/screens/Tab.Library';
 import Dashboard from '@/screens/Tab.Dashboard';
+import SavedStory from '@/screens/Tab.SavedStory';
+import UserProfile from '@/screens/Tab.UserProfile';
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -17,6 +20,9 @@ const AppTab = () => {
   return (
     <Tab.Navigator screenOptions={tabOptions}>
       <Tab.Screen name='Dashboard' component={Dashboard} />
+      <Tab.Screen name='Library' component={Library} />
+      <Tab.Screen name='SavedStory' component={SavedStory} />
+      <Tab.Screen name='UserProfile' component={UserProfile} />
     </Tab.Navigator>
   )
 }
