a20671bec866aeedb7796902fe7a4c6a6c0a49cb
MDEV <tranngocduy.it@gmail.com>
2026-01-08 20:45:53 +0700

    [App] update UI

---
+++ src/assets/svg/index.tsx
@@ -0,0 +1,136 @@
+import { SvgProps } from 'react-native-svg';
+
+import ITabDashboard_SVG from './resource/iTabDashboard.svg';
+export const ITabDashboardSVG = (props: SvgProps) => <ITabDashboard_SVG {...props} />;
+
+import ITabDashboardActive_SVG from './resource/iTabDashboardActive.svg';
+export const ITabDashboardActiveSVG = (props: SvgProps) => <ITabDashboardActive_SVG {...props} />;
+
+import ITabLibrary_SVG from './resource/iTabLibrary.svg';
+export const ITabLibrarySVG = (props: SvgProps) => <ITabLibrary_SVG {...props} />;
+
+import ITabLibraryActive_SVG from './resource/iTabLibraryActive.svg';
+export const ITabLibraryActiveSVG = (props: SvgProps) => <ITabLibraryActive_SVG {...props} />;
+
+import ITabSavedStory_SVG from './resource/iTabSavedStory.svg';
+export const ITabSavedStorySVG = (props: SvgProps) => <ITabSavedStory_SVG {...props} />;
+
+import ITabSavedStoryActive_SVG from './resource/iTabSavedStoryActive.svg';
+export const ITabSavedStoryActiveSVG = (props: SvgProps) => <ITabSavedStoryActive_SVG {...props} />;
+
+import ITabUserProfile_SVG from './resource/iTabUserProfile.svg';
+export const ITabUserProfileSVG = (props: SvgProps) => <ITabUserProfile_SVG {...props} />;
+
+import ITabUserProfileActive_SVG from './resource/iTabUserProfileActive.svg';
+export const ITabUserProfileActiveSVG = (props: SvgProps) => <ITabUserProfileActive_SVG {...props} />;
+
+import IUser_SVG from './resource/iUser.svg';
+export const IUserSVG = (props: SvgProps) => <IUser_SVG {...props} />;
+
+import ILock_SVG from './resource/iLock.svg';
+export const ILockSVG = (props: SvgProps) => <ILock_SVG {...props} />;
+
+import ILockOpen_SVG from './resource/iLockOpen.svg';
+export const ILockOpenSVG = (props: SvgProps) => <ILockOpen_SVG {...props} />;
+
+import IEyeOn_SVG from './resource/iEyeOn.svg';
+export const IEyeOnSVG = (props: SvgProps) => <IEyeOn_SVG {...props} />;
+
+import IEyeOff_SVG from './resource/iEyeOff.svg';
+export const IEyeOffSVG = (props: SvgProps) => <IEyeOff_SVG {...props} />;
+
+import ITotalView_SVG from './resource/iTotalView.svg';
+export const ITotalViewSVG = (props: SvgProps) => <ITotalView_SVG {...props} />;
+
+import ITotalChapter_SVG from './resource/iTotalChapter.svg';
+export const ITotalChapterSVG = (props: SvgProps) => <ITotalChapter_SVG {...props} />;
+
+import IStar_SVG from './resource/iStar.svg';
+export const IStarSVG = (props: SvgProps) => <IStar_SVG {...props} />;
+
+import IStarGray_SVG from './resource/iStarGray.svg';
+export const IStarGraySVG = (props: SvgProps) => <IStarGray_SVG {...props} />;
+
+import IEdit_SVG from './resource/iEdit.svg';
+export const IEditSVG = (props: SvgProps) => <IEdit_SVG {...props} />;
+
+import IEmpty_SVG from './resource/iEmpty.svg';
+export const IEmptySVG = (props: SvgProps) => <IEmpty_SVG {...props} />;
+
+import ISearch_SVG from './resource/iSearch.svg';
+export const ISearchSVG = (props: SvgProps) => <ISearch_SVG {...props} />;
+
+import IFilter_SVG from './resource/iFilter.svg';
+export const IFilterSVG = (props: SvgProps) => <IFilter_SVG {...props} />;
+
+import IBackStack_SVG from './resource/iBackStack.svg';
+export const IBackStackSVG = (props: SvgProps) => <IBackStack_SVG {...props} />;
+
+import IUserAvatar_SVG from './resource/iUserAvatar.svg';
+export const IUserAvatarSVG = (props: SvgProps) => <IUserAvatar_SVG {...props} />;
+
+import IStoryMark_SVG from './resource/iStoryMark.svg';
+export const IStoryMarkSVG = (props: SvgProps) => <IStoryMark_SVG {...props} />;
+
+import IStoryStatus_SVG from './resource/iStoryStatus.svg';
+export const IStoryStatusSVG = (props: SvgProps) => <IStoryStatus_SVG {...props} />;
+
+import IStoryDetail_SVG from './resource/iStoryDetail.svg';
+export const IStoryDetailSVG = (props: SvgProps) => <IStoryDetail_SVG {...props} />;
+
+import IStorySpeech_SVG from './resource/iStorySpeech.svg';
+export const IStorySpeechSVG = (props: SvgProps) => <IStorySpeech_SVG {...props} />;
+
+import IPlayCircle_SVG from './resource/iPlayCircle.svg';
+export const IPlayCircleSVG = (props: SvgProps) => <IPlayCircle_SVG {...props} />;
+
+import IReviewLike_SVG from './resource/iReviewLike.svg';
+export const IReviewLikeSVG = (props: SvgProps) => <IReviewLike_SVG {...props} />;
+
+import IReviewRelay_SVG from './resource/iReviewRelay.svg';
+export const IReviewRelaySVG = (props: SvgProps) => <IReviewRelay_SVG {...props} />;
+
+import ICloseModal_SVG from './resource/iCloseModal.svg';
+export const ICloseModalSVG = (props: SvgProps) => <ICloseModal_SVG {...props} />;
+
+import IArrowFull_SVG from './resource/iArrowFull.svg';
+export const IArrowFullSVG = (props: SvgProps) => <IArrowFull_SVG {...props} />;
+
+import IErrorCircle_SVG from './resource/iErrorCircle.svg';
+export const IErrorCircleSVG = (props: SvgProps) => <IErrorCircle_SVG {...props} />;
+
+import IToastSuccess_SVG from './resource/iToastSuccess.svg';
+export const IToastSuccessSVG = (props: SvgProps) => <IToastSuccess_SVG {...props} />;
+
+import IToastError_SVG from './resource/iToastError.svg';
+export const IToastErrorSVG = (props: SvgProps) => <IToastError_SVG {...props} />;
+
+import ILogout_SVG from './resource/iLogout.svg';
+export const ILogoutSVG = (props: SvgProps) => <ILogout_SVG {...props} />;
+
+import IPage_SVG from './resource/iPage.svg';
+export const IPageSVG = (props: SvgProps) => <IPage_SVG {...props} />;
+
+import IPageSetting_SVG from './resource/iPageSetting.svg';
+export const IPageSettingSVG = (props: SvgProps) => <IPageSetting_SVG {...props} />;
+
+import ICheck_SVG from './resource/iCheck.svg';
+export const ICheckSVG = (props: SvgProps) => <ICheck_SVG {...props} />;
+
+import IPlusCircle_SVG from './resource/iPlusCircle.svg';
+export const IPlusCircleSVG = (props: SvgProps) => <IPlusCircle_SVG {...props} />;
+
+import ISubCircle_SVG from './resource/iSubCircle.svg';
+export const ISubCircleSVG = (props: SvgProps) => <ISubCircle_SVG {...props} />;
+
+import IMediaPlay_SVG from './resource/iMediaPlay.svg';
+export const IMediaPlaySVG = (props: SvgProps) => <IMediaPlay_SVG {...props} />;
+
+import IMediaPause_SVG from './resource/iMediaPause.svg';
+export const IMediaPauseSVG = (props: SvgProps) => <IMediaPause_SVG {...props} />;
+
+import IMediaNext_SVG from './resource/iMediaNext.svg';
+export const IMediaNextSVG = (props: SvgProps) => <IMediaNext_SVG {...props} />;
+
+import IMediaPrevious_SVG from './resource/iMediaPrevious.svg';
+export const IMediaPreviousSVG = (props: SvgProps) => <IMediaPrevious_SVG {...props} />;
