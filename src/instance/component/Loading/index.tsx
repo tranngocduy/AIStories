5f2583a1f8fa1803bd2ded9205cb66d6ffad28a1
MDEV <tranngocduy.it@gmail.com>
2026-01-10 22:41:34 +0700

    [App] update UI

--- src/instance/component/Loading/index.tsx
+++ src/instance/component/Loading/index.tsx
@@ -3,7 +3,7 @@ import { ActivityIndicator, View } from 'react-native';
 
 import styles from './styles';
 
-const Loading: React.FC<{}> = () => {
+const Loading: React.FC = () => {
 
   return <View style={styles.container}><View style={styles.view}><ActivityIndicator size='large' color='#FFFFFF' /></View></View>;
 
